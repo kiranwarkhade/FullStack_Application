330678bb78c31a8de11649c8cbefda710114233c
kiranwarkhade <kiranwarkhade1999@gmial.com>
2025-03-31 14:38:30 +0530

    second commit

--- src/test/java/com/ex/WhatsApp/WhatsAppApplicationTests.java
+++ src/test/java/com/ex/WhatsApp/WhatsAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.ex.WhatsApp;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class WhatsAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "Context loads successfully");
 	}
 
 }
